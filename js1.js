a47ba21084dd6eca2e9dd28aa5602fbaae09e3c3
jaseem1999 <65609243+jaseem1999@users.noreply.github.com>
2020-11-15 21:57:25 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,233 @@
+document.write("hello world");//  printing hello world 
+document.write("<h3>hi ...etc,..<\h3>") // printig  with <h3> line
+console.log("hello console ")// console long 
+var number = 5; // in- line comment
+console.log(number)
+var myName = "jaseem"
+
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14 
+console.log(myName);
+console.log(ourName);
+
+var a;
+var b = 2;
+console.log(b);
+ a = 7;
+ b = a;
+
+ console.log(a);
+
+
+
+ var aa = 5;
+ var bb = 10;
+ var cc = "i am firend "
+ console.log(aa,bb,cc);
+
+ aa = aa + 1;
+ bb = bb + 5;
+ cc = cc + " String!"
+ console.log(aa,bb,cc);
+
+
+ // adding numbers and substration
+
+ var sum = 10 + 10;
+ console.log(sum);
+ 
+ var substration = 10 + 5;
+ console.log(substration);
+
+ // multiplying numbr 
+ var multiplying = 10 * 10;
+ console.log(multiplying);
+
+ // dividing 
+ var dividing = 10 / 10;
+ console.log(dividing);
+
+ // incrementing decrementing
+ var Myvar = 100
+ Myvar++;
+ console.log(Myvar);
+ 
+ var Myvar = 100
+ Myvar--;
+ console.log(Myvar);
+
+ // decimal number 
+var decimal = 5.7
+console.log(decimal);
+var decimal = 0.009
+console.log(decimal);
+
+// decimal multypily
+
+var product = 2.0 * 1.5 ;
+console.log(product);
+
+// decimal dividing
+
+var product = 2.0 / 1.0 ;
+console.log(product);
+
+// findig a reminder   
+ var reminder;
+ reminder = 11 % 3;
+ console.log(reminder);
+
+ // compound assingment with augmented addition
+ var a = 3;
+ var b = 17;
+ var c = 12;
+console.log(a,b,c);
+ a = a + 12;
+ b = 9 + b;
+ c = c + 7;
+ console.log(a,b,c);
+ // a = a + 12;
+ //b = 9 + b;
+ //c = c + 7; same as
+ a += 12;
+ b += 9;
+ c += 7;
+ console.log(a,b,c);
+  // compound assingment with augmented substration
+  var a = 3;
+  var b = 17;
+  var c = 12;
+ console.log(a,b,c);
+  a = a - 12;
+  b = 9 - b;
+  c = c - 7;
+  console.log(a,b,c);
+  // a = a + 12;
+  //b = 9 + b;
+  //c = c + 7; same as
+  a -= 12;
+  b -= 9;
+  c -= 7;
+  console.log(a,b,c);
+   // compound assingment with augmented mutiplycation
+ var a = 3;
+ var b = 17;
+ var c = 12;
+console.log(a,b,c);
+ a = a * 12;
+ b = 9 * b;
+ c = c * 7;
+ console.log(a,b,c);
+ // a = a + 12;
+ //b = 9 + b;
+ //c = c + 7; same as
+ a *= 12;
+ b *= 9;
+ c *= 7;
+ console.log(a,b,c);
+  // compound assingment with augmented divivding
+  var a = 3;
+  var b = 17;
+  var c = 12;
+ console.log(a,b,c);
+  a = a / 12;
+  b = 9 / b;
+  c = c / 7;
+  console.log(a,b,c);
+  // a = a + 12;
+  //b = 9 + b;
+  //c = c + 7; same as
+  a /= 12;
+  b /= 9;
+  c /= 7;
+  console.log(a,b,c);
+
+  // Declare String Variable
+   var FirstName = "adil";
+   var MyLastName = "jaseem";
+   console.log(FirstName , MyLastName);
+
+   var FirstName = "jaseem"
+   var MyLastName = "kalappatt"
+   console.log(FirstName , MyLastName);
+
+   // Escaping Literal Quates in Strings
+   var MyStr = "I am a \"double quated\" String inside \"double quotes\"";
+   console.log(MyStr); 
+
+   // Time 23:45 https://www.youtube.com/watch?v=PkZNo7MFNFg&t=340s
+   // Quoting string with single Quates
+   var MyStr = '<a href="https://www.google.com" target="_blank">link</a>'
+   console.log(MyStr);
+   //Escape in Sequences in strings
+   // Time 23:45
+   //Concatenating Strings with Plus Operator 
+   var MyStr = "this is the start " + "This is the end" ;
+   console.log(MyStr);
+   //Concatenating Strings with Plus Equals Operator 
+   var ourSrt = "I came first.  "
+   ourSrt += "I come Second "
+   console.log(ourSrt);
+   //Constructing String With variable 
+   var ourname = "Adil Jaseem"
+   var ourSrt = "Hello my name is " + ourname + " How are you";
+   console.log(ourSrt);
+
+   // Appanding Variable String 
+   var someadjustive = "hello "
+   var js = "free "
+   js += someadjustive ;
+   console.log(js);
+   //find Length Of String
+   var NameLength = 0;
+   var lastName = " Jassem "
+   NameLength = lastName.length
+   console.log(NameLength);
+   //Bracket Notation to find First Character String 
+   var NameLength = 0;
+   var lastName = "Jassem "
+   NameLength = lastName[0]
+   console.log(NameLength);
+   //String immutability
+   var lostr =  "hi"
+   lostr = "hello"
+   console.log(lostr);
+   //Bracket Notation to find Nth Character in string 
+   var name = "Nappy"
+   var SecondLetterOfName;
+   SecondLetterOfName = name[1];
+   console.log(SecondLetterOfName);//second letter
+   //Bracket Notation to Find Last Character in string 
+   NameLength = name[name.length - 1];
+   console.log(NameLength);
+   
+
+   //35.48 https://www.youtube.com/watch?v=PkZNo7MFNFg&t=1425s
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
